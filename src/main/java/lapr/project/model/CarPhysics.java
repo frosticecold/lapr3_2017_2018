66d2054196f3be6ecb4a5677b102463cd0197aea
Raúl Correia [1090657] <1090657@isep.ipp.pt>
2017-12-12 17:00:05 +0000

    LP31718G19-38 - Initial implementation of base class diagram

--- src/main/java/lapr/project/model/CarPhysics.java
+++ src/main/java/lapr/project/model/CarPhysics.java
@@ -4,22 +4,18 @@ public class CarPhysics {
 
 	public void AppliedForceFlatSurface() {
 		// TODO - implement CarPhysics.AppliedForceFlatSurface
-		throw new UnsupportedOperationException();
 	}
 
 	public void AppliedForceSlope() {
 		// TODO - implement CarPhysics.AppliedForceSlope
-		throw new UnsupportedOperationException();
 	}
 
 	public void LinearVelocity() {
 		// TODO - implement CarPhysics.LinearVelocity
-		throw new UnsupportedOperationException();
 	}
 
 	public void PowerGenerated() {
 		// TODO - implement CarPhysics.PowerGenerated
-		throw new UnsupportedOperationException();
 	}
 
 }
