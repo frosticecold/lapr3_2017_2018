831bcb75db6e25578006a02dcb94b5df8458882e
Raúl Correia [1090657] <1090657@isep.ipp.pt>
2017-12-14 14:34:55 +0000

    LP31718G19-38 - Junction

--- src/main/java/lapr/project/model/Junction.java
+++ src/main/java/lapr/project/model/Junction.java
@@ -1,10 +1,50 @@
 package lapr.project.model;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 public class Junction {
 
-	public Junction() {
-		// TODO - implement Junction.Junction
-		throw new UnsupportedOperationException();
-	}
+    private String m_name;
+    private Set<Road> m_listOfRoads;
+
+    public Junction(String name) {
+        m_name = name;
+        m_listOfRoads = new HashSet<>();
+    }
+
+    public String getName() {
+        return m_name;
+    }
+
+    public void addRoad(Road r) {
+        m_listOfRoads.add(r);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + Objects.hashCode(this.m_name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Junction other = (Junction) obj;
+        if (!Objects.equals(this.m_name, other.m_name)) {
+            return false;
+        }
+        return true;
+    }
 
 }
