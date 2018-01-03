62ceefb6556d6d5f9e1e8531b04c207a08393db0
Miguel Santos [1161386] <1161386@isep.ipp.pt>
2018-01-03 11:40:28 +0000

    [LP31718G19-06] - Electric Vehicle Tests Completed

--- src/test/java/lapr/project/model/VehicleElectricTest.java
+++ src/test/java/lapr/project/model/VehicleElectricTest.java
@@ -22,22 +22,22 @@ import static org.junit.Assert.*;
  * @author Miguel Santos <1161386@isep.ipp.pt>
  */
 public class VehicleElectricTest {
-    
+
     public VehicleElectricTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -111,20 +111,20 @@ public class VehicleElectricTest {
     public void testGetAccelerator() {
         System.out.println("getAccelerator");
         VehicleElectric instance = new VehicleElectric();
-        
+
         List<Regime> regimeList = new LinkedList<>();
-        
+
         Regime r1 = new Regime(10, 20, 4, 8, 1);
         Regime r2 = new Regime(20, 40, 5, 6, 2);
-        Regime r3 = new Regime(19, 29, 1, 7, 3); 
-        
+        Regime r3 = new Regime(19, 29, 1, 7, 3);
+
         regimeList.add(r1);
         regimeList.add(r2);
         regimeList.add(r3);
         Throttle t1 = new Throttle(regimeList);
         Map<Integer, Throttle> throttleMap = new HashMap<>();
         throttleMap.put(25, t1);
-         
+
         Accelerator accelerator = new Accelerator();
         accelerator.setThrottleList(throttleMap);
         instance.setAccelerator(accelerator);
@@ -143,7 +143,7 @@ public class VehicleElectricTest {
         instance.setWheelSize(1.8);
         instance.setFinalDriveRatio(1.2);
         instance.setMaxRPM(200);
-        
+
         List<Gear> gears = new LinkedList<>();
         Gear g1 = new Gear(01, 3);
         Gear g2 = new Gear(02, 2);
@@ -155,144 +155,91 @@ public class VehicleElectricTest {
         gears.add(g4);
         Gearbox gearbox = new Gearbox(gears);
         instance.setGearbox(gearbox);
-        
+
         double expResult = 56.548667764616276;
         double result = instance.getMaximumEngineVelocity();
         assertEquals(expResult, result, 0.0);
     }
-//
-//    /**
-//     * Test of getEnergyRegenerationRatio method, of class VehicleElectric.
-//     */
-//    @Test
-//    public void testGetEnergyRegenerationRatio() {
-//        System.out.println("getEnergyRegenerationRatio");
-//        VehicleElectric instance = new VehicleElectric();
-//        double expResult = 0.0;
-//        double result = instance.getEnergyRegenerationRatio();
-//        assertEquals(expResult, result, 0.0);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of setMinRPM method, of class VehicleElectric.
-//     */
-//    @Test
-//    public void testSetMinRPM() {
-//        System.out.println("setMinRPM");
-//        double m_min_rpm = 0.0;
-//        VehicleElectric instance = new VehicleElectric();
-//        instance.setMinRPM(m_min_rpm);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of setMaxRPM method, of class VehicleElectric.
-//     */
-//    @Test
-//    public void testSetMaxRPM() {
-//        System.out.println("setMaxRPM");
-//        double m_max_rpm = 0.0;
-//        VehicleElectric instance = new VehicleElectric();
-//        instance.setMaxRPM(m_max_rpm);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of setFinalDriveRatio method, of class VehicleElectric.
-//     */
-//    @Test
-//    public void testSetFinalDriveRatio() {
-//        System.out.println("setFinalDriveRatio");
-//        double m_final_drive_ratio = 0.0;
-//        VehicleElectric instance = new VehicleElectric();
-//        instance.setFinalDriveRatio(m_final_drive_ratio);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of setGearbox method, of class VehicleElectric.
-//     */
-//    @Test
-//    public void testSetGearbox() {
-//        System.out.println("setGearbox");
-//        Gearbox m_gearbox = null;
-//        VehicleElectric instance = new VehicleElectric();
-//        instance.setGearbox(m_gearbox);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of setAccelerator method, of class VehicleElectric.
-//     */
-//    @Test
-//    public void testSetAccelerator() {
-//        System.out.println("setAccelerator");
-//        Accelerator m_accelerator = null;
-//        VehicleElectric instance = new VehicleElectric();
-//        instance.setAccelerator(m_accelerator);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of setEnergyRegenerationRatio method, of class VehicleElectric.
-//     */
-//    @Test
-//    public void testSetEnergyRegenerationRatio() {
-//        System.out.println("setEnergyRegenerationRatio");
-//        double m_energy_regeneration_ratio = 0.0;
-//        VehicleElectric instance = new VehicleElectric();
-//        instance.setEnergyRegenerationRatio(m_energy_regeneration_ratio);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of toString method, of class VehicleElectric.
-//     */
-//    @Test
-//    public void testToString() {
-//        System.out.println("toString");
-//        VehicleElectric instance = new VehicleElectric();
-//        String expResult = "";
-//        String result = instance.toString();
+
+    /**
+     * Test of getEnergyRegenerationRatio method, of class VehicleElectric.
+     */
+    @Test
+    public void testGetEnergyRegenerationRatio() {
+        System.out.println("getEnergyRegenerationRatio");
+        VehicleElectric instance = new VehicleElectric();
+        instance.setEnergyRegenerationRatio(0.5);
+        double expResult = 0.5;
+        double result = instance.getEnergyRegenerationRatio();
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of setMinRPM method, of class VehicleElectric.
+     */
+    @Test
+    public void testSetMinRPM() {
+        System.out.println("setMinRPM");
+        double m_min_rpm = 200.0;
+        VehicleElectric instance = new VehicleElectric();
+        instance.setMinRPM(m_min_rpm);
+        double expResult = 200.0;
+        double result = instance.getMinRpm();
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of toString method, of class VehicleElectric.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        VehicleElectric instance = new VehicleElectric();
+        instance.setName("Pick_up");
+        String expResult = "Pick_up";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of validateElectricVehicle method, of class VehicleElectric.
+     */
+    @Test
+    public void testValidateElectricVehicle() {
+        System.out.println("validateElectricVehicle");
+        VehicleElectric instance = new VehicleElectric();
+        instance.setMinRPM(200);
+        instance.setMaxRPM(500);
+        instance.setFinalDriveRatio(1.3);
+        instance.setEnergyRegenerationRatio(0.8);
+        boolean expResult = true;
+        boolean result = instance.validateElectricVehicle();
+        assertEquals(expResult, result);
+
+//        instance.setMaxRPM(100);
+//        expResult = false;
+//        result = instance.validateElectricVehicle();
 //        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
 //
-//    /**
-//     * Test of validateElectricVehicle method, of class VehicleElectric.
-//     */
-//    @Test
-//    public void testValidateElectricVehicle() {
-//        System.out.println("validateElectricVehicle");
-//        VehicleElectric instance = new VehicleElectric();
-//        boolean expResult = false;
-//        boolean result = instance.validateElectricVehicle();
+//        instance.setMaxRPM(500);
+//        instance.setEnergyRegenerationRatio(1.1);
+//        expResult = false;
+//        result = instance.validateElectricVehicle();
 //        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of copy method, of class VehicleElectric.
-//     */
-//    @Test
-//    public void testCopy() {
-//        System.out.println("copy");
-//        VehicleElectric instance = new VehicleElectric();
-//        Vehicle expResult = null;
-//        Vehicle result = instance.copy();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-    
+    }
+
+    /**
+     * Test of copy method, of class VehicleElectric.
+     */
+    @Test
+    public void testCopy() {
+        System.out.println("copy");
+        VehicleElectric instance = new VehicleElectric();
+        instance.setName("pick_up");
+        
+        Vehicle expResult = new VehicleElectric();
+        expResult.setName("pick_up");
+        Vehicle result = instance.copy();
+        assertEquals(expResult, result);
+    }
 }
